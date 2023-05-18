f720d3f36603d0236eabf46b7c96ce7f9e18e430
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-05-18 06:44:48 +0200

    terminamos de copiar el codigo del aula virtual

--- ejercicio.py
+++ ejercicio.py
@@ -55,3 +55,71 @@ async def download(session, uri):
     with open(uri.split(sep)[-1], "wb") as f:  
         f.write(content)  
         return uri 
+
+
+
+async def get_images_src_from_html(html_doc):  
+    soup = BeautifulSoup(html_doc, "html.parser")  
+    for img in soup.find_all('img'):  
+        yield img.get('src')  
+        await asyncio.sleep(0.001) 
+
+
+
+async def get_uri_from_images_src(base_uri, images_src):  
+    parsed_base = urlparse(base_uri)  
+    async for src in images_src:  
+        parsed = urlparse(src)  
+        if parsed.netloc == '':  
+            path = parsed.path  
+            if parsed.query:  
+                path += '?' + parsed.query  
+            if path[0] != '/':  
+                if parsed_base.path == '/':  
+                    path = '/' + path  
+                else:  
+                    path = '/' + '/'.join(parsed_base.path.split('/')[:-1]) + '/' + path  
+                    yield parsed_base.scheme + '://' + parsed_base.netloc + path  
+        else:  
+            yield parsed.geturl()  
+        await asyncio.sleep(0.001) 
+
+
+
+async def get_images(session, page_uri):  
+    html = await wget(session, page_uri)  
+    if not html:  
+        print("Error: no se ha encontrado ninguna imagen", sys.stderr)  
+        return None  
+    images_src_gen = get_images_src_from_html(html)  
+    images_uri_gen = get_uri_from_images_src(page_uri, images_src_gen)  
+    async for image_uri in images_uri_gen:  
+        print('Descarga de %s' % image_uri)  
+        await download(session, image_uri)
+
+
+
+async def main():  
+    web_page_uri = 'http://www.formation-python.com/'  
+    async with aiohttp.ClientSession() as session:  
+        await get_images(session, web_page_uri)
+
+
+
+asyncio.run(main()) 
+
+
+
+def write_in_file(filename, content):   
+    with open(filename, "wb") as f:   
+        f.write(content) 
+
+
+
+async def download(session, uri):  
+    content = await wget(session, uri)  
+    if content is None:  
+        return None  
+    loop = asyncio.get_running_loop()  
+    await loop.run_in_executor(None, partial(write_in_file, uri.split(sep)[-1], content))  
+    return uri 
